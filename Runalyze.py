cd8fa593044c1e5f9296ee39c7c9ad8e03108ee8
Pon Dinesh Kumar M <mp17@medtronic.com>
2025-01-13 16:29:51 +0530

    did many things

--- Runalyze.py
+++ Runalyze.py
@@ -1,130 +1,134 @@
 import os
-
 import streamlit as st
 from streamlit_echarts import st_echarts
 import requests
 from dotenv import load_dotenv
-
-load_dotenv('access.env')
-
+import this
 
 st.set_page_config(layout="wide")
+load_dotenv('access.env')
 st.title("Azure DevOps Test Run Analysis")
 cols = st.columns(8)
- 
-class_keywords = { 
-    "Selenium Exceptions": [
-        "InvalidCastException", "InvalidOperationException", "NoSuchElementException",
-        "NotImplementedException", "WebDriverTimeoutException", "NullReferenceException",
-        "WebDriverException", "FormatException", "BindingException", "JsonReaderException",
-        "TimeoutException", "AggregateException", "ComparisonException", "XmlException",
-        "ElementNotInteractableException", "HttpRequestException", "NoSuchWindowException",
-        "ArgumentNullException", "ApiException", "InvalidElementStateException", "StaleElementReferenceException",
-        "DirectoryNotFoundException"
-    ],
-    "Custom Exceptions": [
-        "Cities belonging to initiating clinic as well as Specialist clinics are displayed",
-        "Data Export failed", "Unexpected page displayed", "No pull record found",
-        "No commands available to post", "Collection was modified; enumeration operation may not execute",
-        "Row with clinic name", "No patient with key 0 exists",
-        "PDF generation failed", "No emails matching the search were found", "No clinic with key 0 exists",
-        "Patient is not displayed on Patient Assignment page", "InstrumentCommandsResponse is null", "Transmission not found", "HeartFailureManagementLink does not exists",
-        "No Archived Transmissions are displayed on the Transmissions List Page", "Tranmission with iTransmissionId", "Transmission not found",
-        "Unable to retrieve EarliestAppointmentDate for patient", "Patient with DSN"
-    ],
-    "Assert Failed": [
-        "Assert.IsTrue", "Assert.Fail", "Assert.AreEqual", "Assert.IsNotNull", "Assert.IsFalse", "StringAssert.Contains",
-        "CollectionAssert.AreEquivalent", "Assert.Inconclusive"
-    ],
-    "Data Setup API Error": [
-        "Data Setup API error InternalServerError (500)", "Data Setup API error Unauthorized (401)", "Data Setup API error NotFound (404)", "DataSetupService timedout" 
-    ],
-    "Carelink API Error": [
-        "Carelink API error"
-    ]
-}
+unclassified_errors = []
 
-unclassified_errors = [] 
+
+
+
+def custom_metric(label, value, color, font_size="16px"):
+    return f"""
+    <div style="border:2px solid {color}; border-radius:10px; padding:10px; margin:5px;">
+        <h2 style="color:{color}; font-size:{font_size}; margin:0; line-height:1.2; padding: 0;">{label}</h2>
+        <h1 style="color:{color}; font-size:calc({font_size} * 2); margin:0; line-height:1.2; padding: 0;">{value}</h1>
+    </div>
+    """
 
 def classify_error_message(error_message):
-    for class_label, keywords in class_keywords.items():
+    for class_label, keywords in this.class_keywords.items():
         if any(keyword in error_message for keyword in keywords):
             return class_label
     unclassified_errors.append(error_message)
     return "Unclassified"
 
-def fetch_run_results(run_id):
+def initialize(run_id):
+    #GETTING RUN RESULT
     url = f"https://dev.azure.com/MDTProductDevelopment/carelink/_apis/test/Runs/{run_id}/results?api-version=5.0"
     access_token = os.getenv('ACCESS_TOKEN')
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
-    
-    response = requests.get(url, headers=headers, verify=False)  
-    if response.status_code == 200:
-        return response.json()
-    else:
-        st.error(f"Failed to retrieve data for run ID {run_id}. Status Code: {response.status_code}")
-        return None
-
-
-
-def fetch_test_run_summary(run_id):
-    url = f"https://dev.azure.com/MDTProductDevelopment/carelink/_apis/test/Runs/{run_id}?api-version=5.0"
-    access_token = os.getenv('ACCESS_TOKEN')
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-    }
     response = requests.get(url, headers=headers, verify=False)
-    if response.status_code == 200:
-        summary = response.json()
-        run_statistics = summary.get("runStatistics", [])
-
-        # Initialize counts
-        failed_count = 0
-        aborted_count = 0
-
-        for stat in run_statistics:
-            if stat.get("outcome") == "Failed":
-                failed_count = stat.get("count", 0)
-            elif stat.get("outcome") == "None":
-                aborted_count = stat.get("count", 0)
-
-        return {
-            "total": summary.get("totalTests", 0),
-            "passed": summary.get("passedTests", 0),
-            "failed": failed_count,
-            "aborted": aborted_count
-        }
+    if response.ok:
+        this.run_result = response.json()
     else:
-        st.error(f"Failed to retrieve summary for run ID {run_id}. Status Code: {response.status_code}")
-        return None
+        this.run_result = {}
 
-def display_metrics(run_id):
-    summary = fetch_test_run_summary(run_id)
-    if not summary:
-        return
+    #GETTING RUN SUMMARY
+    url = f"https://dev.azure.com/MDTProductDevelopment/carelink/_apis/test/Runs/{run_id}?api-version=5.0"
+    response = requests.get(url, headers=headers, verify=False)
+    if response.ok:
+        this.run_summary = response.json()
+    else:
+        this.run_summary = {}
+
+    #SETTING GLOBAL VARIABLES
+    this.run_name = this.run_summary.get("name", "N/A")
+    this.run_url = this.run_summary.get("url", "#")
+    this.build_name = this.run_summary.get("build", {}).get("name", "N/A")
+    this.build_url = this.run_summary.get("build", {}).get("url", "#")
+    this.state = this.run_summary.get("state", "N/A")
+    this.start_date = this.run_summary.get("startedDate", "N/A")
+    this.end_date = this.run_summary.get("completedDate", "N/A")
+    this.release_stage = this.run_summary.get("releaseEnvironmentUri", "N/A")
+    this.total = this.run_summary.get("totalTests", 0)
+    this.passed = this.run_summary.get("passedTests", 0)
+    this.failed = next(
+        (stat.get("count", 0) for stat in this.run_summary.get("runStatistics", []) if stat.get("outcome") == "Failed"), 0)
+    this.aborted = next(
+        (stat.get("count", 0) for stat in this.run_summary.get("runStatistics", []) if stat.get("outcome") == "None"), 0)
+
+
+def display_run_details(font_size='14px'):
+
+
+    custom_css = f"""
+    <style>
+        .metric-label {{
+            font-size: {font_size};
+            font-weight: bold;
+        }}
+        .metric-value {{
+            font-size: calc({font_size} * 1.5);
+            margin-bottom: 0.75rem;
+        }}
+        .metric-link {{
+            font-size: calc({font_size} * 1.5);
+            margin-bottom: 0.75rem;
+        }}
+        .metric-link a {{
+            color: #007bff; /* Bootstrap default blue */
+            text-decoration: underline;
+        }}
+    </style>
+    """
+
+    st.markdown(custom_css, unsafe_allow_html=True)
+
+    st.markdown(f"""
+        <div class="metric-label">Run ID</div>
+        <div class="metric-value">{this.run_id}</div>
+        <div class="metric-label">Run Name</div>
+        <div class="metric-link"><a href="{this.run_url}" target="_blank">{this.run_name}</a></div>
+        <div class="metric-label">Build Name</div>
+        <div class="metric-link"><a href="{this.build_url}" target="_blank">{this.build_name}</a></div>
+        <div class="metric-label">State</div>
+        <div class="metric-value">{this.state}</div>
+        <div class="metric-label">Start Date</div>
+        <div class="metric-value">{this.start_date}</div>
+        <div class="metric-label">End Date</div>
+        <div class="metric-value">{this.end_date}</div>
+        <div class="metric-label">Release Stage</div>
+        <div class="metric-link"><a href="{this.release_stage}" target="_blank">{this.release_stage}</a></div>
+    """, unsafe_allow_html=True)
+
+
+
+
+def analyze_and_plot():
+
     with cols[4]:
-        st.metric("Total Tests", f"{summary['total']}", border=True)
+        st.markdown(custom_metric("Total Tests", this.total, "grey", "14px"), unsafe_allow_html=True)
     with cols[5]:
-        st.metric("Passed Tests", f"{summary['passed']}", border=True)
+        st.markdown(custom_metric("Passed Tests", this.passed, "green", "14px"), unsafe_allow_html=True)
     with cols[6]:
-        st.metric("Failed Tests", f"{summary['failed']}", border=True)
+        st.markdown(custom_metric("Failed Tests", this.failed, "red", "14px"), unsafe_allow_html=True)
     with cols[7]:
-        st.metric("Aborted Tests", f"{summary['aborted']}", border=True)
+        st.markdown(custom_metric("Aborted Tests", this.aborted, "orange", "14px"), unsafe_allow_html=True)
 
-
-def analyze_and_plot(run_id):
-    data = fetch_run_results(run_id)
-    
-    if not data:
-        return
-    
-    classification_counts = {key: 0 for key in class_keywords.keys()}
+    classification_counts = {key: 0 for key in this.class_keywords.keys()}
     classification_counts["Unclassified"] = 0
-    keyword_counts = {key: {kw: 0 for kw in keywords} for key, keywords in class_keywords.items()}
+    keyword_counts = {key: {kw: 0 for kw in keywords} for key, keywords in this.class_keywords.items()}
 
-    for result in data.get('value', []):
+    for result in this.run_result.get('value', []):
         outcome = result.get('outcome', None)
         if outcome == "Failed":
             error_message = result.get('errorMessage', '')
@@ -140,7 +144,7 @@ def analyze_and_plot(run_id):
     option_pie = {
         "title": {
             "text": "Failure Classification",
-            "subtext": f"Run ID: {run_id}",
+            "subtext": f"Run ID: {this.run_id}",
             "left": "center",
             "textStyle": {"color": "#FFFFFF", "fontWeight": "bold"}
         },
@@ -148,17 +152,16 @@ def analyze_and_plot(run_id):
             "trigger": "item",
             "formatter": "{b}: {d}%"
         },
-         
         "legend": {
-             "orient": "vertical",
-             "left": "right",
-             "textStyle": {
-                    "color": "#FFFFFF",
-                    "fontSize": 10
-                },
-             "backgroundColor": "rgba(50, 50, 50, 0.7)",
-             "borderRadius": 5,
-             "padding": [10, 10, 10, 10]
+            "orient": "vertical",
+            "left": "right",
+            "textStyle": {
+                "color": "#FFFFFF",
+                "fontSize": 10
+            },
+            "backgroundColor": "rgba(50, 50, 50, 0.7)",
+            "borderRadius": 5,
+            "padding": [10, 10, 10, 10]
         },
         "series": [
             {
@@ -167,13 +170,13 @@ def analyze_and_plot(run_id):
                 "radius": "50%",
                 "data": chart_data,
                 "label": {
-                "normal": {
-                    "textStyle": {
-                        "color": "#FFFFFF",
-                        "fontWeight": "bold"
+                    "normal": {
+                        "textStyle": {
+                            "color": "#FFFFFF",
+                            "fontWeight": "bold"
+                        }
                     }
-                }
-            },
+                },
                 "emphasis": {
                     "itemStyle": {
                         "shadowBlur": 10,
@@ -186,17 +189,12 @@ def analyze_and_plot(run_id):
     }
     pie_chart_container = st.container()
     with pie_chart_container:
-        # Define the columns within the container
-        col1, col2, col3 = st.columns([1, 1, 3])
-        # Merge the middle columns for the pie chart
+        col1, col2 = st.columns([2, 3])
         with col1:
-            pass  # This could also be an empty placeholder or pass
+            display_run_details()
         with col2:
-            pass  # Empty column to be merged with the pie chart
-        with col3:
-            st_echarts(option_pie, height="500px")  # This is where the pie chart is placed
+            st_echarts(option_pie, height="650px")
 
-    
 
     for class_label, keywords in keyword_counts.items():
         if any(keywords.values()):
@@ -229,7 +227,7 @@ def analyze_and_plot(run_id):
                     "axisLabel": {
                         "color": "#FFFFFF",
                         "fontWeight": "bold",
-                        "interval": 0, 
+                        "interval": 0,
                     }
                 },
                 "series": [
@@ -249,15 +247,12 @@ def analyze_and_plot(run_id):
             st_echarts(options=option_bar, key=class_label, height="500px")
 
     if unclassified_errors:
-     st.subheader("Unclassified Errors:")
-     for error in unclassified_errors:
-      st.text(error)
-
+        st.subheader("Unclassified Errors:")
+        for error in unclassified_errors:
+            st.text(error)
 
 with cols[0]:
-    run_id = st.text_input("Enter the run ID:")
+    this.run_id = st.text_input("Enter the run ID:")
 if st.button("Analyze"):
-    display_metrics(run_id)
-    analyze_and_plot(run_id)
-
-  
+    initialize(this.run_id)
+    analyze_and_plot()
---
+++ this.py
@@ -0,0 +1,50 @@
+
+class_keywords = {
+    "Selenium Exceptions": [
+        "InvalidCastException", "InvalidOperationException", "NoSuchElementException",
+        "NotImplementedException", "WebDriverTimeoutException", "NullReferenceException",
+        "WebDriverException", "FormatException", "BindingException", "JsonReaderException",
+        "TimeoutException", "AggregateException", "ComparisonException", "XmlException",
+        "ElementNotInteractableException", "HttpRequestException", "NoSuchWindowException",
+        "ArgumentNullException", "ApiException", "InvalidElementStateException", "StaleElementReferenceException",
+        "DirectoryNotFoundException"
+    ],
+    "Custom Exceptions": [
+        "Cities belonging to initiating clinic as well as Specialist clinics are displayed",
+        "Data Export failed", "Unexpected page displayed", "No pull record found",
+        "No commands available to post", "Collection was modified; enumeration operation may not execute",
+        "Row with clinic name", "No patient with key 0 exists",
+        "PDF generation failed", "No emails matching the search were found", "No clinic with key 0 exists",
+        "Patient is not displayed on Patient Assignment page", "InstrumentCommandsResponse is null", "Transmission not found", "HeartFailureManagementLink does not exists",
+        "No Archived Transmissions are displayed on the Transmissions List Page", "Tranmission with iTransmissionId", "Transmission not found",
+        "Unable to retrieve EarliestAppointmentDate for patient", "Patient with DSN"
+    ],
+    "Assert Failed": [
+        "Assert.IsTrue", "Assert.Fail", "Assert.AreEqual", "Assert.IsNotNull", "Assert.IsFalse", "StringAssert.Contains",
+        "CollectionAssert.AreEquivalent", "Assert.Inconclusive"
+    ],
+    "Data Setup API Error": [
+        "Data Setup API error InternalServerError (500)", "Data Setup API error Unauthorized (401)", "Data Setup API error NotFound (404)", "DataSetupService timedout"
+    ],
+    "Carelink API Error": [
+        "Carelink API error"
+    ]
+}
+
+run_result = None
+run_summary = None
+
+run_name = None
+run_url = None
+build_name = None
+build_url = None
+state = None
+start_date = None
+end_date = None
+release_stage = None
+
+run_id = None
+total = 0
+passed = 0
+failed = 0
+aborted = 0
